345228464545361b99f146ef91626a6f415cb4d9
Chincheron <jmhyde891@gmail.com>
2025-02-06 21:38:18 -0500

    Add comment and formatting

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
