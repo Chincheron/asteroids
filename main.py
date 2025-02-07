f4fd98d3bd4d72f53013aafc88abadbad3d2a9a3
Chincheron <jmhyde891@gmail.com>
2025-02-06 21:36:30 -0500

    Setup initial clock logic and cap to 60 FPS

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +16,7 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
